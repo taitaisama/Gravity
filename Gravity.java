17d583b0d6335ea56f26619c3b296360f181c7bd
Ramanuj <69118312+taitaisama@users.noreply.github.com>
2021-06-04 18:13:45 +0530

    Delete Gravity.java

--- Gravity.java
+++
@@ -1,8 +0,0 @@
-package gravity;
-public class Gravity {
-    static Board b=new Board();
-    public static void main(String[] args) {
-        System.setProperty("sun.java2d.opengl", "true");
-        b.setVisible(true);
-    }
-}
